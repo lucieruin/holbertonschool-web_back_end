e18ad429b91642488ec6e36976d35affdcb8a941
lucieruin <lucie.wattez@hotmail.fr>
2024-01-04 14:22:00 +0100

    change the start ane end tile for loop

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -12,13 +12,13 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """ coroutine that will execute async_comprehension four times
     in parallel using asyncio.gather"""
-    start_time = time.time()
+    start_time = asyncio.get_event_loop().time()
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-    end_time = time.time()
+    end_time = asyncio.get_event_loop().time()
     total_runtime = end_time - start_time
     return total_runtime
