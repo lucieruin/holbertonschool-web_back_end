a9a88de6027a8be00f002d8d822b89add30ce67b
lucieruin <lucie.wattez@hotmail.fr>
2024-01-04 10:25:58 +0100

    add comments

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" async routine called wait_n that takes in 2 int arguments """
+""" async routine called wait_n that takes in 2 int arguments
+wait_n should return the list of all the delays (float values).
+The list of the delays should be in ascending order """
 
 from typing import List
 import asyncio
@@ -10,6 +12,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ return the list of all the delays in ascending order """
     tasks = [wait_random(max_delay) for _ in range(n)]
-    delay = await asyncio.gather(*tasks)
+    delays = await asyncio.gather(*tasks)
 
-    return sorted(delay)
+    return sorted(delays)
