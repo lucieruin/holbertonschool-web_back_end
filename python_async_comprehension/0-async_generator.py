0062b10f5035da0a8d4172fbdca4458cd589eaf4
lucieruin <lucie.wattez@hotmail.fr>
2024-01-04 13:35:54 +0100

    add comment

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" coroutine called async_generator that takes no arguments """
+""" coroutine called async_generator that takes no arguments
+The coroutine will loop 10 times, each time asynchronously wait 1 second,
+then yield a random number between 0 and 10"""
 
 
 import asyncio
