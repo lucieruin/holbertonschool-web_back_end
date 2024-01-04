3ad84bbc8202545d784e87ecec9dd0b61d29df90
lucieruin <lucie.wattez@hotmail.fr>
2024-01-04 10:59:24 +0100

    delete async for def and pycostyle correct

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -9,12 +9,11 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """ measures the total execution time for wait_n(n, max_delay) """
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
-
     total_time = end_time - start_time
-    
+
     return total_time / n
