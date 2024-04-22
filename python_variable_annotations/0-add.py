3d0c891d86e862e05b656d90e5ac658780a0dfa1
lucieruin <lucie.wattez@hotmail.fr>
2024-04-22 10:57:15 +0200

    add function add, concat and floor for basics annotation

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" add 2 floats """
+
+
+def add(a: float, b: float) -> float:
+    """ add 2 floats """
+    return (a + b)
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" function floor which takes a float n as argument
+and returns the floor of the float """
+
+import math
+
+
+def floor(n: float) -> int:
+    """ def returns the floor """
+    return math.floor(n)
