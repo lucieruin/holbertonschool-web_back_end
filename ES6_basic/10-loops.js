7b271ea80140fb805e627d52c0db0cf5f28a80d9
lucieruin <lucie.wattez@hotmail.fr>
2024-05-07 13:51:56 +0200

    use ES6’s for...of operator for return a newarray

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const index of array) {
+    newArray.push(appendString + index);
+  }
+  return newArray;
+}
