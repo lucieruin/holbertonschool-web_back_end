eedc3d13638c7585a796cf330d5e918feb6ca62f
lucieruin <lucie.wattez@hotmail.fr>
2024-05-21 17:16:35 +0200

    returns an array of students for a specific city with their new grade

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(ids, city, newGrades) {
+  return ids
+    .filter((id) => id.location === city)
+    .map((student) => {
+      const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeObject ? gradeObject.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
